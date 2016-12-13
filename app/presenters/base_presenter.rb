4620801c6bdb6a06c618bb96882a7f3f3b7045ee
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2016-12-13 18:54:38 +0200

    Add methods to base_presenter

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,4 +1,14 @@
 class BasePresenter
+  @build_attributes = []
+
+  class << self
+    attr_accessor :build_attributes
+
+    def build_with(*args)
+      @build_attributes = args.map(&:to_s)
+    end
+  end
+
   attr_accessor :object, :params, :data
 
   def initialize(object, params, options={})
