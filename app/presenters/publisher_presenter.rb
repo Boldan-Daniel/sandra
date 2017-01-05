61e27ac8b3d4301188afbdfcd3ae11c626a5ffc4
Daniel Boldan <daniel.boldan@clever-software-solutions.com>
2017-01-05 20:33:24 +0200

    Add eager loader query builder

--- app/presenters/publisher_presenter.rb
+++ app/presenters/publisher_presenter.rb
@@ -1,4 +1,4 @@
-class PublisherPresenter
+class PublisherPresenter < BasePresenter
   related_to :books
   sort_by    :id, :name, :created_at, :updated_at
   filter_by  :id, :name, :created_at, :updated_at
