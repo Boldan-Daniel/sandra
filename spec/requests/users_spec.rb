ce48d06874500da432066a40719d4746e361d81d
Daniel Boldan <boldan.daniel@gmail.com>
2017-02-12 02:49:03 +0200

    Remove describe user embed fields

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -65,34 +65,6 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-    xdescribe 'embed picking' do
-      context 'with valid "embed" parameter' do
-        before { get '/api/users?embed=author' }
-
-        it 'gets the users with their authors embedded' do
-          json_body['data'].each do |user|
-            expect(user['author'].keys).to eq(['id', 'given_name', 'family_name', 'created_at', 'updated_at'])
-          end
-        end
-      end
-
-      context 'with invalid "embed" relation "fake"' do
-        before { get '/api/users?embed=fake,author' }
-
-        it 'gets "400 Bad Request" back' do
-          expect(response.status).to eq 400
-        end
-
-        it 'receives an error' do
-          expect(json_body['error']).to_not be nil
-        end
-
-        it 'receives "fields=fid" as an invalid param' do
-          expect(json_body['error']['invalid_params']).to eq 'embed=fake'
-        end
-      end
-    end
-
     describe 'pagination' do
       context 'when asking for the first page' do
         before { get '/api/users?page=1&per=1' }
