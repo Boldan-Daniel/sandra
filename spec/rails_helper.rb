1e595b807edf4ef94db78d8129d1e5667d6a3ed5
Daniel Boldan <boldan.daniel@gmail.com>
2017-02-13 03:28:28 +0200

    WebMock configuration - allow connection to stripe.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'webmock/rspec'
 require 'database_cleaner'
 require 'pundit/rspec'
+require 'stripe'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -56,6 +57,9 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+# Allow stripe
+WebMock.disable_net_connect!(allow: 'https://api.stripe.com')
 
 # Configure VCR
 VCR.configure do |config|
