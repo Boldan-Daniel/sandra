f6669ef6e6f9a872823655fb9bb357c4caab3fb3
Daniel Boldan <boldan.daniel@gmail.com>
2017-02-19 10:25:54 +0200

    Caching paginator

--- app/query_builders/paginator.rb
+++ app/query_builders/paginator.rb
@@ -55,4 +55,29 @@ class Paginator
   def show_last_link?
     @scope.total_pages > 1 && !@scope.last_page?
   end
+
+  def last_page?
+    return true unless last_updated_at
+
+    key = "qb/p/#{@scope.model}/#{last_updated_at.to_datetime}/last_page?"
+
+    Rails.cache.fetch(key) do
+      !@scope.last_page?
+    end
+  end
+
+  def total_pages
+    return 1 unless last_updated_at
+
+    key = "qb/p/#{@scope.model}/#{last_updated_at.to_datetime}/count"
+
+    Rails.cache.fetch(key) do
+      @scope.total_pages
+    end
+  end
+
+  def last_updated_at
+    @last_updated_at ||= @scope.unscoped
+                             .order('updated_at DESC').first.try(:updated_at)
+  end
 end
